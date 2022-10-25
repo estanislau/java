9c959b2b96926a555a4f967993a8c7b7385f6881
adama <adama@macos>
2022-10-25 20:19:31 -0300

    Novos .java

---
+++ Matriz3.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mo_220922;
+
+/**
+ *
+ * @author adama
+ */
+public class Matriz3 {
+    
+}
